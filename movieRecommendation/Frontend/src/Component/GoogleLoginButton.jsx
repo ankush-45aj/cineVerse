1cf4649345ed02e432d59be280d1c4b9a0c2151f
Ankush <jhaankush47@gmail.com>
2026-01-15 02:54:02 +0530

    build logs

--- movieRecommendation/Frontend/src/Component/GoogleLoginButton.jsx
+++ movieRecommendation/Frontend/src/Component/GoogleLoginButton.jsx
@@ -10,26 +10,26 @@ const GoogleLoginButton = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const handleLoginSuccess = async(response) => {
+    const handleLoginSuccess = async (response) => {
         const decoded = jwtDecode(response.credential);
         console.log(decoded.picture); // You can use this information to log the user in.
-        const username= decoded.given_name;
+        const username = decoded.given_name;
         await dispatch(loginUser({ username })).then((action) => {
-        if (rememberMe) {
-            Cookies.set('loggedIn', true, { expires: 1 }); // Cookie expires in 1 day
-            Cookies.set('username', decoded.given_name, { expires: 1 }); // Cookie expires in 1 day
-            Cookies.set('profilePic', decoded.picture, { expires: 1 }); // Cookie expires in 1 day
-        }
-              dispatch(setProfilePic(decoded.picture));
-              // dispatch(setUsername(email));
-              navigate("/home");
-              // dispatch(sendEmail({ email }));
+            if (rememberMe) {
+                Cookies.set('loggedIn', true, { expires: 1 }); // Cookie expires in 1 day
+                Cookies.set('username', decoded.given_name, { expires: 1 }); // Cookie expires in 1 day
+                Cookies.set('profilePic', decoded.picture, { expires: 1 }); // Cookie expires in 1 day
+            }
+            dispatch(setProfilePic(decoded.picture));
+            // dispatch(setUsername(email));
+            navigate("/home");
+            // dispatch(sendEmail({ email }));
         });
 
         // Perform necessary login actions here
-        
+
         // dispatch(setUsername(decoded.given_name));
-        
+
         // dispatch(login());
         // navigate('/home');
 
